4667b67e583e011732804438f76d3aee25a6c041
raza123981 <l201005@lhr.nu.edu.pk>
2023-04-28 20:24:38 +0500

    added toast in main activity

--- hoteldo/app/src/main/java/com/hoteldo/app/MainActivity.java
+++ hoteldo/app/src/main/java/com/hoteldo/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.hoteldo.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
 
 }
