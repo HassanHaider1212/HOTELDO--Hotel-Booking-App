b8d65cf267ccb828b4d942c5c687156b9c66e34e
HassanHaider1212 <119353034+HassanHaider1212@users.noreply.github.com>
2023-04-28 20:57:10 +0500

    updated

--- hoteldo/app/src/main/java/com/hoteldo/app/MainActivity.java
+++ hoteldo/app/src/main/java/com/hoteldo/app/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        Toast.makeText(this, "Modified by zain", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Modified by hassan", Toast.LENGTH_SHORT).show();
     }
 
 }
