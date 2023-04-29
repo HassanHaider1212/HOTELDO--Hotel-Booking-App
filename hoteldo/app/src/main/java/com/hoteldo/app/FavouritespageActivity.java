1bde5979c2f966b58de3cef2fce6be640acc8cbc
Zain Ahmad <zainwajid33@gmail.com>
2023-04-29 12:47:02 +0500

    Create FavouritespageActivity.java
    Created basic Favouritespage activity along with bottom navigation bar implementation

---
+++ hoteldo/app/src/main/java/com/hoteldo/app/FavouritespageActivity.java
@@ -0,0 +1,46 @@
+package com.hoteldo.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class FavouritespageActivity extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favouritespage);
+
+        BottomNavigationView navbar = (BottomNavigationView) findViewById(R.id.favouritesnavbar);
+        navbar.setSelectedItemId(R.id.nav_favourite);
+
+        navbar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.nav_favourite:
+                        return true;
+                    case R.id.nav_home:
+                        startActivity(new Intent(getApplicationContext(),HomepageActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_orderhistory:
+                        startActivity(new Intent(getApplicationContext(),OrderhistorypageActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_profile:
+                        startActivity(new Intent(getApplicationContext(),ProfilepageActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
